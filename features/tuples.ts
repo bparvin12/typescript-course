f3aa007237a40ac3ef75db9c3d4b2a932a915dda
Bijan Parvin <bparvin12@gmail.com>
2023-10-09 14:59:48 -0700

    feat: section 6 tuples

---
+++ features/tuples.ts
@@ -0,0 +1,22 @@
+const drink = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+};
+
+// Type alias
+type Drink = [string, boolean, number];
+
+const pepsi: Drink = ['brown', true, 40];
+const sprite: Drink = ['clear', true, 40];
+const tea: Drink = ['brown', false, 0];
+
+const carSpecs: [number, number] = [400, 3354];
+
+// in general, its easier to model using an object and more intuitive than using a tuple
+const carStats = {
+  horsepower: 400,
+  weight: 3354,
+};
+
+
