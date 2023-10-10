07b4caf9c7d4ca88bf23069fd5e19d9fd398a2f3
Bijan Parvin <bparvin12@gmail.com>
2023-10-10 09:42:34 -0700

    feat: section 7 interfaces

---
+++ features/interfaces.ts
@@ -0,0 +1,64 @@
+// interface: creates a new type, describing the 
+// property names and value types of an object
+
+interface Vehicle {
+  name: string;
+  year: Date;
+  broken: boolean;
+  summary(): string;
+}
+interface Reportable {
+  summary(): string;
+}
+
+const oldCivic = {
+  name: 'civic',
+  year: new Date(),
+  broken: true,
+  summary(): string {
+    return `Name: ${this.name}`;
+  } 
+};
+
+const drink2 = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `My drink has ${this.sugar} grams of sugar`
+  }
+}
+
+// before interface
+// const printVehicle = (vehicle: {
+//   name: string;
+//   year: number;
+//   broken: boolean;
+// }): void => {
+//   console.log(`Name: ${vehicle.name}`);
+//   console.log(`Year: ${vehicle.year}`);
+//   console.log(`Broken: ${vehicle.broken}`);
+// };
+
+// printVehicle(oldCivic);
+
+// after interface
+const printVehicle = (vehicle: Vehicle): void => {
+  console.log(`Name: ${vehicle.name}`);
+    console.log(`Year: ${vehicle.year}`);
+    console.log(`Broken: ${vehicle.broken}`)
+};
+printVehicle(oldCivic);
+
+
+const printSummary = (item: Reportable): void => {
+  console.log(item.summary());
+};
+printSummary(oldCivic);
+
+printSummary(drink2);
+
+
+
+
+
