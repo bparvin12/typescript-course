cd8e864fb1209753b1b20cca06c43502118c7012
Bijan Parvin <bparvin12@gmail.com>
2023-10-11 14:26:33 -0700

    feat: section 10 ~ abstract classes vs interfaces

---
+++ features/classes_vs_interfaces.ts
@@ -0,0 +1,11 @@
+/* 
+  Interfaces
+  - Sets up a contract between different classes 
+  - use when we have a very different objects that we want to work together
+  - promotes loose coupling
+
+  Inheritance/Abstract Classes
+  - Sets up a contract between difference classes
+  - use when we are trying to build up a definition of an object 
+  - storngly couples classes together
+*/
